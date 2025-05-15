fcdee6936df2a52a90ce93ae618b0da6a91890fd
Paola <paola.landaeta.lv@gmail.com>
2025-05-15 19:33:14 +0200

    W

--- js/galeria.js
+++ js/galeria.js
@@ -1,41 +1,35 @@
-let imagenes = Array.from({length: 7}, (_, i) => `../Imagenes/img${i + 1}.jpg`);
 let indiceActual = 0;
 let imagen = document.querySelector(".galeria-carrusel img");
 
-function mostrarImagen(indice) {
-    imagen.src = imagenes[indice];
-}
+// Cargar archivo JSON
+fetch('../imagenes.json')
+  .then(response => response.json())
+  .then(data => {
+    let imagenes = data.imagenes.map(imagen => imagen.ruta);
 
-document.addEventListener("DOMContentLoaded", function() {
+    function mostrarImagen(indice) {
+      imagen.src = imagenes[indice];
+    }
+
     mostrarImagen(indiceActual);
-});
 
-document.querySelector(".flecha-izquierda").addEventListener("click", () => {
-    indiceActual = (indiceActual - 1 + imagenes.length) % imagenes.length;
-    mostrarImagen(indiceActual);
-});
+    document.querySelector(".flecha-izquierda").addEventListener("click", () => {
+      indiceActual = (indiceActual - 1 + imagenes.length) % imagenes.length;
+      mostrarImagen(indiceActual);
+    });
 
-document.querySelector(".flecha-derecha").addEventListener("click", () => {
-    indiceActual = (indiceActual + 1) % imagenes.length;
-    mostrarImagen(indiceActual);
-});
+    document.querySelector(".flecha-derecha").addEventListener("click", () => {
+      indiceActual = (indiceActual + 1) % imagenes.length;
+      mostrarImagen(indiceActual);
+    });
+  })
+  .catch(error => console.error('Error:', error));
 
+// Agregar clase active al enlace de navegación
 const currentPage = window.location.pathname;
 const navLinks = document.querySelectorAll('nav ul li a');
-
 navLinks.forEach((link) => {
   if (link.href.includes(currentPage)) {
     link.classList.add('active');
   }
 });
-
-document.addEventListener('DOMContentLoaded', () => {
-    const currentPage = window.location.pathname;
-    const navLinks = document.querySelectorAll('nav ul li a');
-  
-    navLinks.forEach((link) => {
-      if (link.href.includes(currentPage)) {
-        link.classList.add('active');
-      }
-    });
-  });
